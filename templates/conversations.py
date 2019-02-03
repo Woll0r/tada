c23e1a0aa587452e7b988ca68a67e8d60e3771ac
Scott McClaugherty <gamefreak4321@gmail.com>
2019-02-03 17:32:28 -0500

    Add support for HD emotes to the conversations pack.

--- templates/conversations.py
+++ templates/conversations.py
@@ -2,6 +2,9 @@
 
 import json
 import zipfile
+from os.path import isfile
+from PIL import Image
+from io import BytesIO
 
 from yapsy.IPlugin import IPlugin
 
@@ -21,12 +24,22 @@ class ConversationsBackend(IPlugin):
         emotes = {}
         for emote in self.pack.emotelist:
             try:
+                dpiLevels = None
+                if isfile(self.pack.path + "/hidpi/" + emote.filename) and not emote.filename.endswith(".gif"):
+                    with Image.open(self.pack.path + "/" + emote.filename) as orig:
+                        origSize = orig.size;
+                    self.perform_resize(emote, origSize, 2, outzip)
+                    self.perform_resize(emote, origSize, 3, outzip)
+                    dpiLevels = [2, 3]
+
                 outzip.write(self.pack.path+"/"+emote.filename, "emotes/" + emote.filename)
                 emotes[emote.filename] = {
                     "aliases": emote.shortcuts,
                     "width": emote.width,
                     "height": emote.height
                 }
+                if dpiLevels is not None:
+                    emotes[emote.filename]["hidpi"] = dpiLevels
             except OSError:
                 # The underlying emote file isn't found
                 # This throws varying errors, but are all OSError or subclasses
@@ -39,3 +52,10 @@ class ConversationsBackend(IPlugin):
             "emotes": emotes
         }))
         outzip.close()
+
+    def perform_resize(self, emote, origSize, scale, outzip):
+        """Resize and add the HD versions"""
+        name, ext = emote.filename.split('.')
+        with Image.open(self.pack.path + "/hidpi/" + emote.filename ) as highres, BytesIO() as f:
+            highres.resize([origSize[0] * scale, origSize[1] * scale], resample=Image.LANCZOS).save(f, format=ext)
+            outzip.writestr("emotes/%s@%dx.%s" % (name, scale, ext), f.getvalue())
